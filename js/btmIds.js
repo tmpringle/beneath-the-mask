4b7145ace795b07f69a1ec7d7a8b2fdac1d7b232
Tyler McMillian Pringle <tmpringle@wm.edu>
2022-09-05 21:08:36 -0400

    New video for clear nighttime The old video for clear/cloudy nights no longer plays on external sites, so I've added a new one that works (for now.)

--- js/btmIds.js
+++ js/btmIds.js
@@ -1,7 +1,7 @@
 // video ids for beneath the mask versions
 var vidIds = [
     'gGLwGn9XdBE', // day theme, 6:00 - 17:59
-    'tx-6-wAid70', // night theme, 18:00 - 5:59
+    '_wXzLHzbTo0', // night theme, 18:00 - 5:59
     '5gX77ttIpyw', // rainy/snowy day theme
     'uY7ngS9QxMg'  // rainy/snowy night theme
 ]
