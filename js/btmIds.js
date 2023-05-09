30a73986a21d5929ee92ce8023350be244a15423
tmpringle <pringletyler1@gmail.com>
2023-05-09 15:07:02 -0400

    New videos for rain versions
    The old videos for the rain versions no longer play on external sites,
    so I've added new ones that work (for now.)

--- js/btmIds.js
+++ js/btmIds.js
@@ -2,6 +2,6 @@
 var vidIds = [
     'gGLwGn9XdBE', // day theme, 6:00 - 17:59
     '_wXzLHzbTo0', // night theme, 18:00 - 5:59
-    '5gX77ttIpyw', // rainy/snowy day theme
-    'uY7ngS9QxMg'  // rainy/snowy night theme
+    'QpMn2auHhFA', // rainy/snowy day theme
+    'S19AbEz9rQU'  // rainy/snowy night theme
 ]
