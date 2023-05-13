efa20f84ee7dcbbc932ed47c2f2ebfeeb573ec46
tmpringle <pringletyler1@gmail.com>
2023-05-12 21:10:46 -0400

    An EXTREMELY rough attempt at rendering calendar
    I'm able to render the month and date using a canvas (not the day of the week yet), but the actual proportions and orientations of the numbers and backgrounds are off. Also, the way I'm loading in the images is not the most efficient. Nonetheless, I've shown that a calendar element is feasible. I still need to implement functionality for having the calendar graphic change when the date changes.

--- js/calendar.js
+++ js/calendar.js
@@ -17,32 +17,98 @@ function getCurrentDay() {
 function displayCalendar() {
     let month = getCurrentMonth();
     let date = getCurrentDate();
-    let dayOfWeek;
-    
-    switch(getCurrentDay()) {
-        case 0:
-            dayOfWeek = 'Sunday';
-            break;
-        case 1:
-            dayOfWeek = 'Monday';
-            break;
-        case 2:
-            dayOfWeek = 'Tuesday';
-            break;
-        case 3:
-            dayOfWeek = 'Wednesday';
-            break;
-        case 4:
-            dayOfWeek = 'Thursday';
-            break;
-        case 5:
-            dayOfWeek = 'Friday';
-            break;
-        case 6:
-            dayOfWeek = 'Saturday';
-            break;
-    }
-    
-    let dateString = dayOfWeek + ', ' + month + '/' + date;
-    document.getElementById('calendar').innerHTML = document.getElementById('calendar').innerHTML.replace('Date Placeholder', dateString);
+    let dayOfWeek = getCurrentDay();
+
+    drawCalendar(month, date, dayOfWeek);
 }
+
+function drawCalendar(month, date, dayOfWeek) {
+    const backCanvas = document.getElementById("background-canvas");
+    const backContext = backCanvas.getContext("2d");
+
+    const midCanvas = document.getElementById("middle-canvas");
+    const midContext = midCanvas.getContext("2d");
+
+    const foreCanvas = document.getElementById("foreground-canvas");
+    const foreContext = foreCanvas.getContext("2d");
+
+    let dateDigit1;
+    let dateDigit2;
+
+    if (date >= 10) {
+        dateDigit1 = Math.floor(date / 10);
+        dateDigit2 = date % 10;
+    } else {
+        dateDigit1 = date;
+    }
+
+/*    if (canvas.getContext) {
+        const context = canvas.getContext("2d");
+    } else { // canvas is not supported by the browser, so don't worry about it
+        return;
+    }
+*/
+
+/* for now, before i come up with a better system, i could make functions for
+   the months, dates, and days of the week that take in an image, a context,
+   and a pixel position
+*/
+
+    const monthDigitF = new Image();
+    monthDigitF.onload = function() {
+        foreContext.drawImage(monthDigitF, 19, 24);
+    };
+    monthDigitF.src = `images/calendar/months/m${month}f.png`;
+
+    const monthDigitM = new Image();
+    monthDigitM.onload = function() {
+        midContext.drawImage(monthDigitM, 9, 15);
+    };
+    monthDigitM.src = `images/calendar/months/m${month}m.png`;
+
+    const monthDigitB = new Image();
+    monthDigitB.onload = function() {
+        backContext.drawImage(monthDigitB, 0, 10);
+    };
+    monthDigitB.src = `images/calendar/months/m${month}b.png`;
+
+    const dateDigit1F = new Image();
+    dateDigit1F.onload = function() {
+        foreContext.drawImage(dateDigit1F, 83, 14);
+    };
+    dateDigit1F.src = `images/calendar/dates/${dateDigit1}f.png`;
+
+    const dateDigit1M = new Image();
+    dateDigit1M.onload = function() {
+        midContext.drawImage(dateDigit1M, 65, 5);
+    };
+    dateDigit1M.src = `images/calendar/dates/${dateDigit1}m.png`;
+
+    const dateDigit1B = new Image();
+    dateDigit1B.onload = function() {
+        backContext.drawImage(dateDigit1B, 55, 0);
+    };
+    dateDigit1B.src = `images/calendar/dates/${dateDigit1}b.png`;
+
+    if (date >= 10) {
+        const dateDigit2F = new Image();
+        dateDigit2F.onload = function() {
+            foreContext.drawImage(dateDigit2F, 128, 14);
+        };
+        dateDigit2F.src = `images/calendar/dates/${dateDigit2}f.png`;
+        
+        const dateDigit2M = new Image();
+        dateDigit2M.onload = function() {
+            midContext.drawImage(dateDigit2M, 115, 5);
+        };
+        dateDigit2M.src = `images/calendar/dates/${dateDigit2}m.png`;
+        
+        const dateDigit2B = new Image();
+        dateDigit2B.onload = function() {
+            backContext.drawImage(dateDigit2B, 106, 0);
+        };
+        dateDigit2B.src = `images/calendar/dates/${dateDigit2}b.png`;
+    }
+}
+
+// TODO: implement checking for date changes
