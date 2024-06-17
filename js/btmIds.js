3ec4b004b88e78bfb7a219e183403979f1a12521
tmpringle <pringletyler1@gmail.com>
2024-06-17 17:19:15 -0400

    New video for clear daytime

--- js/btmIds.js
+++ js/btmIds.js
@@ -1,6 +1,6 @@
 // video ids for beneath the mask versions
 var vidIds = [
-    'gGLwGn9XdBE', // day theme, 6:00 - 17:59
+    'Ut-KaUQNuts', // day theme, 6:00 - 17:59
     '_wXzLHzbTo0', // night theme, 18:00 - 5:59
     'QpMn2auHhFA', // rainy/snowy day theme
     'S19AbEz9rQU'  // rainy/snowy night theme
